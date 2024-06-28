c04c926e631e98d58ad3b2507352e936e8bc05b2
menshibin <men_shi_bin@163.com>
2024-06-28 14:14:01 +0800

    modify test case

--- src/main/java/com/taosdata/flink/Main.java
+++ src/main/java/com/taosdata/flink/Main.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args)  throws Exception {
-        if (args != null && args.length > 0 &&  args[0] == "init") {
+        if (args != null && args.length > 0 &&  args[0].equals("init")) {
             initTable();
         }else {
             insertData();
